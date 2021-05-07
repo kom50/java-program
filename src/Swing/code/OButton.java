4ca6d994ce250b4d8e50a074e3665b2f859519c0
kom50 <omprakashartik82280@gmail.com>
2021-05-07 19:04:22 +0530

    Gradient and round Button design class

--- src/Swing/code/OButton.java
+++ src/Swing/code/OButton.java
@@ -1,35 +1,60 @@
 package Swing.code;//package Swing.code;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class OButton extends JButton {
-    int radius;
+public class OButton extends JButton implements MouseListener{
+    int radius, radius1;
     Color startColor = Color.BLACK, endColor = Color.white;
 
     int startX, startY, endX, endY;
-
+    boolean isFill = true;
+    void setFillButton(boolean b){
+        isFill = b;
+    }
+    Color pressColor = Color.lightGray,
+            enterColor = Color.lightGray, foregroundColor = Color.WHITE,
+            HoverForegroundColor = Color.WHITE;
+    boolean isGradient   = true;
     public OButton(){
+        this.setContentAreaFilled(false); // fill pain content
+        this.setBorder(new EmptyBorder(0, 0, 1, 0));
+        this.setFocusable(false);
         startX = startY = 11;
-        radius = 0;
+        radius1 = radius = 0;
         endX = endY = 120;
         startColor = Color.BLACK;
         endColor = Color.white;
+        this.addMouseListener(this);
     }
 
     public OButton(String name){
         super(name);
-        radius = 0;
+        this.setContentAreaFilled(false); // fill pain content
+        this.setBorder(new EmptyBorder(0, 0, 1, 0));
+        this.setFocusable(false);
+        radius = radius1 = 0;
         startX = startY = 1;
-        endX = 70;
-        endY = 20;
+        endX = 130;
+        endY = 90;
         startColor = Color.BLACK;
         endColor = Color.white;
+        this.addMouseListener(this);
     }
 
+    public void setGradient(boolean b){ this.isGradient = b;  }
+    public void setPressedColor(Color color) { this.pressColor = color; }
+    public void setHoverColor(Color color) { this.enterColor = color; }
+    public void setHoverForegroundColor(Color color){ this.HoverForegroundColor = color; }
+    public void setForegroundColor(Color color){
+        this.foregroundColor = color;
+        this.setForeground(foregroundColor);
+    }
     public void setStartX(int startX) {
         this.startX = startX;
     }
-
     public void setStartY(int startY) {
         this.startY = startY;
     }
@@ -42,9 +67,13 @@ public class OButton extends JButton {
         this.endY = endY;
     }
 
-
     public void setBorderRadius(int radius){
         this.radius = radius;
+        this.radius1 = radius;
+    }
+    public void setBorderRadius(int radius, int radius1){
+        this.radius = radius;
+        this.radius1 = radius1;
     }
     public  void setStartColor(Color color){
         startColor = color;
@@ -53,17 +82,106 @@ public class OButton extends JButton {
         endColor = color;
     }
 
+    Graphics2D graphics2D;
+    GradientPaint pain1;
+    boolean mouseClick = false, mouseOut = false, mouseEnter = false, mouseExit = false;
+
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2 = (Graphics2D) g;
-        GradientPaint pain1 =new  GradientPaint(startX, startY,startColor, endX, endY, endColor);
-        System.out.println("Hello paint () "+startX+" "+startY+" "+endX+" "+endY);
-        g2.setPaint(pain1);
-        g2.fillRoundRect(0, 0, OButton.this.getWidth(),OButton.this.getHeight(), this.radius, this.radius);
-        super.paint(g2);
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics2D = (Graphics2D) graphics;
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if(!isGradient){ // if isGradient is false then button is filled with one color startColor
+            graphics.setColor(startColor);
+            if (mouseClick) {
+                mouseClick = false;
+                graphics.setColor(pressColor);
+            }
+            if (mouseOut) {
+                mouseOut = false;
+                graphics.setColor(startColor);
+            }
+            if (mouseEnter) {
+                mouseEnter = false;
+                this.setForeground(HoverForegroundColor);
+                graphics.setColor(enterColor);
+            }
+            if (mouseExit) {
+                mouseExit = false;
+                this.setForeground(foregroundColor);
+                graphics.setColor(startColor);
+            }
+            System.out.println("Hello paint () " + startX + " " + startY + " " + endX + " " + endY);
+            System.out.println("Gradient option " + isGradient);
+            if (isFill)
+                graphics.fillRoundRect(0, 0, OButton.this.getWidth(), OButton.this.getHeight(), this.radius, this.radius1);
+            else
+                graphics.drawRoundRect(0, 0, OButton.this.getWidth() - 1, OButton.this.getHeight() - 1, this.radius, this.radius1);
+            super.paint(graphics);
+        }else {
+            pain1 = new GradientPaint(startX, startY, startColor, endX, endY, endColor);
+            if (mouseClick) {
+                mouseClick = false;
+                pain1 = new GradientPaint(startX, startY, pressColor, endX, endY, pressColor);
+            }
+            if (mouseOut) {
+                mouseOut = false;
+                pain1 = new GradientPaint(startX, startY, startColor, endX, endY, endColor);
+            }
+            if (mouseEnter) {
+                mouseEnter = false;
+                this.setForeground(HoverForegroundColor);
+                pain1 = new GradientPaint(startX, startY, enterColor, endX, endY, enterColor);
+            }
+            if (mouseExit) {
+                this.setForeground(foregroundColor);
+                mouseExit = false;
+                pain1 = new GradientPaint(startX, startY, startColor, endX, endY, endColor);
+            }
+            System.out.println("Hello paint () " + startX + " " + startY + " " + endX + " " + endY);
+            graphics2D.setPaint(pain1);
+            System.out.println("Gradient option " + isGradient);
+
+            if (isFill)
+                graphics2D.fillRoundRect(0, 0, OButton.this.getWidth(), OButton.this.getHeight(), this.radius, this.radius1);
+            else
+                graphics2D.drawRoundRect(0, 0, OButton.this.getWidth() - 1, OButton.this.getHeight() - 1, this.radius, this.radius1);
+            super.paint(graphics2D);
+        }
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Mouse pressed");
+        mouseClick = true;
+        repaint();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("Mouse Released");
+        mouseOut = true;
+        repaint();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("Mouse Entered");
+        mouseEnter = true;
+        repaint();
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("Mouse Exited");
+        mouseExit = true;
+        repaint();
+    }
 
 
     public static void main(String[] args) {
@@ -76,26 +194,40 @@ public class OButton extends JButton {
         frame.setBackground(Color.white);
 
         OButton com = new OButton();
-        com.setContentAreaFilled(false); // fill pain content   
-        com.setBorder(null);
         com.setText("Java Om");
         com.setFont(new Font("Arial", Font.BOLD, 20));
         com.setForeground(Color.CYAN);
-        com.setFocusable(false);
-
-        com.setEndColor(Color.BLUE);
-        com.setStartColor(Color.YELLOW);
+        com.setBorderRadius(10);
+        com.setEndColor(Color.green);
+        com.setStartColor(Color.blue);
 //        com.setStartX(10);
 //        com.setStartY(10);
-//        com.setEndY(100);
-//        com.setEndX(40);
+        com.setEndY(100);
+        com.setEndX(40);
 
 
         com.setBounds(100, 100, 180, 50);
         com.setBorderRadius(20);
+        com.setHoverColor(Color.BLUE);
+        com.setForegroundColor(Color.BLACK);
+        com.setFillButton(false);
 
         com.addActionListener( event -> System.out.println("Button clicked"));
         frame.getContentPane().add(com);
+        OButton btn = new OButton("JAVA");
+        btn.setBounds(140, 190, 100, 60);
+        frame.getContentPane().add(btn);
+        btn.setForeground(Color.BLUE);
+        btn.setFillButton(true);
+        btn.setFont(new Font("Arail", Font.BOLD, 18));
+        btn.setBorderRadius(50, 110);
+//        btn.setHoverColor(Color.red);
+//        btn.setGradient(false);
+        btn.setStartColor(Color.BLUE);
+
+        btn.setForeground(Color.getHSBColor(10, 30, 40));
+        btn.setHoverForegroundColor(Color.YELLOW);
+        btn.setHoverColor(Color.CYAN);
 
         frame.getContentPane().setBackground(Color.white);
     }
---
+++ src/Swing/code/OPanel.java
@@ -0,0 +1,25 @@
+package Swing.code;
+import javax.swing.*;
+import java.awt.*;
+public class OPanel extends JPanel {
+    Color startColor = Color.BLACK, endColor = Color.white;
+    int width = 100, height = 100;
+    public void setStartColor(Color startColor) {
+        this.startColor = startColor;
+    }
+    public void setEndColor(Color endColor) {
+        this.endColor = endColor;
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D graphics2D = (Graphics2D) g;
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        height = getHeight();
+        width  = getWidth();
+        Paint paint = new GradientPaint(0.0f, 0.0f, startColor, width, height, endColor, true);
+        graphics2D.setPaint(paint);
+        graphics2D.fillRect(0, 0, getWidth(), getHeight());
+    }
+}
+
